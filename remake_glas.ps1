21830eb10a1e3d980df234c292bb47862b002622
The Chosen Goose <xtremebass2k@gmail.com>
2024-05-23 20:44:17 -0700

    fix that memory leak

--- remake_glas.ps1
+++ remake_glas.ps1
@@ -1,9 +1,9 @@
 Get-ChildItem -Recurse -Path .\src_html\glas\* -Include index.html | ForEach-Object { 
-    Write-Output & C:/Users/choosegoose/.virtualenvs/html_to_tscn-UVkTTcz6/Scripts/python.exe c:/code/html_to_tscn/page_content.py --src $_.FullName
+	& C:/Users/choosegoose/.virtualenvs/html_to_tscn-UVkTTcz6/Scripts/python.exe c:/code/html_to_tscn/page_content.py --src $_.FullName
 }
 
 # Get-ChildItem -Recurse -Path .\src_html\glas\page-28\* -Include index.html | ForEach-Object { 
-#     Write-Output & C:/Users/choosegoose/.virtualenvs/html_to_tscn-UVkTTcz6/Scripts/python.exe c:/code/html_to_tscn/page_content.py --src $_.FullName
+#     & C:/Users/choosegoose/.virtualenvs/html_to_tscn-UVkTTcz6/Scripts/python.exe c:/code/html_to_tscn/page_content.py --src $_.FullName
 # }
 # & C:/Users/choosegoose/.virtualenvs/html_to_tscn-UVkTTcz6/Scripts/python.exe c:/code/html_to_tscn/generate_main.py
 
